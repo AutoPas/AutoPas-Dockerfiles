6a4fdb7d72c9e37e019f0f5f17e7a4c96bc9211a
seckler <seckler@in.tum.de>
2020-11-10 13:12:20 +0100

    adds dockerfile for oneapi.

---
+++ buildenv/intel-one-api/bin/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+ccache_default_dir=$(ccache -s | head -n 1 | rev | cut -d ' ' -f1 | rev | sed "s|//|/ccache_default_dir/|")
+suffix=$(lscpu | sed -nr '/Model name/ s/.*:\s*(.*)/\1/p' | sed -e 's/[^A-Za-z0-9._-]/_/g')
+
+
+export CCACHE_DIR=${ccache_default_dir}/${suffix}
+
+mkdir -p $CCACHE_DIR
+
+exec "$@"
